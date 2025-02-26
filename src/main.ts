85b4ce658a5feb7f46367573ef64cf3e1bbac868
Mid1i <michac.tchernov@yandex.ru>
2025-02-26 23:24:26 +0300

    feat: Подключена `Pinia`

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 
 import { router } from "@/router";
 import App from "@/App.vue";
@@ -7,7 +8,8 @@ import "@/assets/styles/base.scss";
 
 
 const Vue = createApp(App);
+const Pinia = createPinia();
 
+Vue.use(Pinia);
 Vue.use(router);
-
 Vue.mount('#app');
