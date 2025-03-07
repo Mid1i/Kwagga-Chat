cd70755fe252c50f64a9905b3632707703fd938b
Mid1i <michac.tchernov@yandex.ru>
2025-03-07 14:00:09 +0300

    feat: Добавлен функционал для выбранного чата

--- src/store/chat.ts
+++ src/store/chat.ts
@@ -1,15 +1,22 @@
 import { defineStore } from "pinia";
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 
 import type { IChat } from "@/interfaces/Chat";
 import { CHATS } from "@/test";
 
 
 export const useChatStore = defineStore("chats", () => {
+	const currentChat = ref<IChat | null>(null);
 	const chats = reactive<IChat[]>(CHATS);
+
+	const setCurrentChat = (id: number) => {
+		currentChat.value = chats.find(el => el.id === id) ?? null;
+	};
 
 
 	return {
-		chats
+		chats,
+		currentChat,
+		setCurrentChat
 	}
 });
