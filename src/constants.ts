f18d057804b1bc4c2d2c4a4785bb84fa1da60e21
Mid1i <michac.tchernov@yandex.ru>
2025-03-07 13:58:43 +0300

    feat: Добавлены константы для дат и времени

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,23 @@ export const KEYBOARD_ACTIONS: Record<string, (container: HTMLElement) => number
 	PageDown: (container) => container.scrollTop += container.clientHeight,
 	PageUp: (container) => container.scrollTop -= container.clientHeight
 };
+
+// Дни недели по порядковым номерам
+export const WEEK_DAYS: Record<number, string> = {
+	0: "Вс",
+	1: "Пн",
+	2: "Вт",
+	3: "Ср",
+	4: "Чт",
+	5: "Пт",
+	6: "Сб"
+};
+
+// День в милисекундах
+export const DAY_MS = 24 * 60 * 60 * 1000;
+
+// Неделя в милисекундах
+export const WEEK_MS = 7 * DAY_MS;
+
+// Год в милисекундах
+export const YEAR_MS = 365 * DAY_MS;
