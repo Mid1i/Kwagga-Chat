ca1c36825727e1f9a9865b7f571df9c54845b49c
Mid1i <michac.tchernov@yandex.ru>
2025-03-07 14:00:25 +0300

    fix: Оптимизированы функции форматирования

--- src/helpers/datetime.ts
+++ src/helpers/datetime.ts
@@ -1,8 +1,14 @@
+import { WEEK_DAYS } from "@/constants";
+
+
 // Добавление ведущих нулей
 export const leadingZeros = (num: string | number): string => num.toString().padStart(2, "0");
 
 // Форматирование даты (времени) в формат "10.01.2025" (11:30)
-export const formattedDatetime = (date: Date, separator: string) => {
+export const formattedDatetime = (date: Date, separator: string): string => {
 	const dateArray = (separator === ":") ? [date.getHours(), date.getMinutes()] : [date.getDate(), date.getMonth() + 1, date.getFullYear()];
 	return dateArray.map(leadingZeros).join(separator);
 };
+
+// Форматирование дня недели (по его порядковому номеру)
+export const formattedWeekDay = (date: Date): string => WEEK_DAYS[date.getDay()];
