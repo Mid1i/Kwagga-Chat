4486963ef05eaf296ae67f503b809969c624af80
Mid1i <michac.tchernov@yandex.ru>
2025-03-11 14:13:51 +0300

    fix: Изменена структура

--- src/composables/useAPI.ts
+++ src/composables/useAPI.ts
@@ -2,36 +2,26 @@ import { ref } from "vue";
 import axios from "axios";
 
 import type { StatusAPI, MethodAPI } from "@/types";
-import type { AxiosResponse } from "axios";
 
 import { API_URL } from "@/constants";
 
 
 export default function useAPI<T>(initialData: T) {
 	const data = ref<T>(initialData);
-	const status = ref<StatusAPI>("loading");
+	const status = ref<StatusAPI>("idle");
 
 
-	const fetchData = async (
-		url: string, 
-		method: MethodAPI, 
-		params: Record<string, any> = {}
-	) => {
+	const fetchData = async (url: string, method: MethodAPI, body?: Record<string, any>) => {
 		status.value = "loading";
 
-		axios({ method, url: `${API_URL}${url}`, params })
-			.then(handleData)
-			.catch(handleError)
-	};
-
-	const handleData = (response: AxiosResponse) => {
-		data.value = response.data;
-		status.value = "success"; 
-	};
-
-	const handleError = (error: string) => {
-		console.error(error);
-		status.value = "error";
+		try {
+			const response = await axios({ method, url: `${API_URL}${url}`, data: body });
+			data.value = response.data;
+			status.value = "success";
+		} catch(error) {
+			console.error(error);
+			status.value = "error";
+		}
 	};
 
 
