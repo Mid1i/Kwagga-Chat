e9864e2c7714014ae8a8565ec9c73ef80bbca330
Mid1i <michac.tchernov@yandex.ru>
2025-03-12 22:02:52 +0300

    fix: Временно убран запрос получения истории сообщений

--- src/stores/chat.ts
+++ src/stores/chat.ts
@@ -5,25 +5,43 @@ import type { IUnsentMessage, IHistory, IChat, ICurrentChat } from "@/types";
 
 import { generateColor } from "@/helpers/color";
 import useAPI from '@/composables/useAPI';
+import { HISTORY } from "@/test";
 
 
 export const useChatStore = defineStore("chats", () => {
 	const { data: chats, status: chatsStatus, fetchData } = useAPI<IChat[]>([]);
-	const { data: history, status: historyStatus, fetchData: fetchHistory } = useAPI<IHistory[]>([]);
+	// const { data: history, status: historyStatus, fetchData: fetchHistory } = useAPI<IHistory[]>([]);
+	
+	const { status: historyStatus, fetchData: fetchHistory } = useAPI<IHistory[]>([]);
+	const history = ref<IHistory[]>(HISTORY);
 
 	const currentChat = ref<ICurrentChat | null>(null);
+	const isOpen = ref<boolean>(false);
 
 	const unsentMessages = reactive<IUnsentMessage[]>([]);
 
+	const toggleChat = () => {
+		isOpen.value = !isOpen.value;
+	};
+
 	const setCurrentChat = (id: number) => {
 		const selected = chats.value.find(el => el.id === id);
-		if (selected) currentChat.value = { ...selected, color: generateColor(selected.recepient.username) };
+		
+		if (selected) {
+			toggleChat();
+			currentChat.value = { 
+				...selected, 
+				color: generateColor(selected.recepient.username) 
+			}
+		}
 	};
 
 	const updateUnsentMessages = (text: string) => {
+		if (!currentChat.value) return;
+
 		unsentMessages.push({
 			id: unsentMessages.length,
-			chatId: currentChat.value!.id,
+			chatId: currentChat.value.id,
 			text
 		});
 	};
@@ -41,6 +59,8 @@ export const useChatStore = defineStore("chats", () => {
 
 
 	return {
+		isOpen,
+		toggleChat,
 		chats,
 		chatsStatus,
 		history,
