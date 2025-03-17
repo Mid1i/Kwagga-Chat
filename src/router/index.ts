4b708165b83a0c7b6b27e3ae9f1d7110c1c09765
Mid1i <michac.tchernov@yandex.ru>
2025-03-17 23:17:00 +0300

    feat: Добавлена проверка на авторизованного пользователя

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { storeToRefs } from "pinia";
+
+import { useAuth } from "@/store";
 import { routes } from "./routes";
 
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes
+});
+
+router.beforeEach((to, _, next) => {
+	const { isAuthenticated } = storeToRefs(useAuth());
+
+	const isWelcome = to.name === "welcome";
+	const authenticated = isAuthenticated.value;
+
+	if (isWelcome && authenticated) next({ name: "index" });
+
+	(!isWelcome && to.name !== "callback" && !authenticated) ? next({ name: "welcome" }) : next();
 });
 
 export default router;
