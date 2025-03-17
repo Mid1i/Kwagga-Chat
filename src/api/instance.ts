5536d46df73619300aa492f2a69b857f129620ab
Mid1i <michac.tchernov@yandex.ru>
2025-03-17 23:17:54 +0300

    feat: Добавлено отслеживание просроченности токена

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,11 +1,28 @@
 import axios from "axios";
 
 import { API_URL } from "@/constants";
+import { useAuth } from "@/store";
 
 
 const api = axios.create({
 	baseURL: API_URL,
 	timeout: 10000
+});
+
+api.interceptors.request.use(async config => {
+	const { keycloak, logout } = useAuth();
+
+	try {
+		if (keycloak) {
+			await keycloak.updateToken(60);
+			config.headers["Authorization"] = `Bearer ${keycloak.token}`;
+		}
+	} catch (error) {
+		console.log("Ошибка обновления токена: ", error);
+		logout();
+	}
+
+	return config;
 });
 
 
