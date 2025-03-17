6d278b4005da2ae231eeefc311c5dfc131aa80f1
Mid1i <michac.tchernov@yandex.ru>
2025-03-17 23:17:14 +0300

    feat: Добавлен компонент всплывающего меню

---
+++ src/components/chat/PopupMenu.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+	import type { IMenuItem } from "@/types";
+
+	import CustomButton from "@/components/ui/CustomButton.vue";
+
+
+	defineProps<{ 
+		customClass?: string;
+		customStyle?: Record<string, string>;
+		label: string;
+		items: IMenuItem[]; 
+	}>();
+</script>
+
+
+<template>
+	<nav 
+		:class="['menu', customClass]" 
+		:style="{ ...customStyle }"
+		:aria-label="label"
+	>
+		<CustomButton
+			v-for="item in items"
+			:key="item.icon"
+			type="nav"
+			:="item"
+		/>
+	</nav>
+</template>
+
+
+<style lang="scss" scoped>
+	.menu {
+		@include flex-column;
+		@include popup;
+
+		left: 15px;
+		top: 55px;
+	}
+
+
+	@media(max-width: 767px) {
+		.menu {
+			top: 50px;
+		}
+	}
+</style>
