74d8469e7e17233659f3e8179f05266a6c471bd5
Mid1i <michac.tchernov@yandex.ru>
2025-03-18 20:07:46 +0300

    fix: Небольшие изменения

--- src/composables/useStomp.ts
+++ src/composables/useStomp.ts
@@ -1,45 +1,51 @@
-import { ref, onMounted, onUnmounted } from "vue";
-import { Client } from "@stomp/stompjs";
+import { reactive, onMounted, onUnmounted } from "vue";
+import { Client, IMessage, IFrame } from "@stomp/stompjs";
 
-const useStomp = (url: string, publish: string, subscribe: string) => {
-  const messages = ref<string[]>([]);
+
+const useStomp = (brokerURL: string, destination: string, subscribe: string) => {
+  const messages = reactive<string[]>([]);
+  const errors = reactive<IMessage[]>([]);
+  
   let stompClient: Client;
 
-	const sendMessage = (msg: string) => {
-		const body = JSON.stringify({ message: msg });
-    if (stompClient && stompClient.connected) stompClient.publish({ 
-			destination: publish, 
-			headers: {
-				"recepient-id": "3d42469a-ab64-49f9-98f8-a27f0e8dc5d8"
-			},
-			body: body 
+
+	const sendMessage = (message: string) => {
+    if (!stompClient || !stompClient.connected) return;
+    
+    stompClient.publish({ 
+			destination, 
+			headers: { "recepient-id": "" },
+			body: JSON.stringify({ message }) 
 		});
+  };
+
+  const onConnect = () => {
+    console.log("Connected to STOMP");
+
+    // Подписываемся на чат с пользователем
+    stompClient.subscribe(subscribe, message => messages.push(message.body));
+
+    // Подписываемся на ошибки
+    stompClient.subscribe(`/user/.../errors`, error => errors.push(error));
+  };
+
+  const onDisconnect = () => {
+    console.log("Disconnected from STOMP");
+  };
+
+  const onStompError = (error: IFrame) => {
+    console.log(error)
   };
 
 
   onMounted(() => {
     stompClient = new Client({
-      brokerURL: url,
-			connectHeaders: {
-				Authorization: `Bearer `
-			},
+      brokerURL: brokerURL,
+			connectHeaders: { Authorization: `Bearer ` },
       reconnectDelay: 100000,
-      onConnect: () => {
-        console.log("Connected to STOMP");
-        stompClient.subscribe(subscribe, (message) => {
-          messages.value.push(message.body);
-					console.log(message.body)
-        });
-				stompClient.subscribe("/user/602db6a0-fb19-4cfb-a9bd-aa5d9cc8cd96/errors", (message) => {
-					console.log(message.body)
-        });
-      },
-      onDisconnect: () => {
-        console.log("Disconnected from STOMP");
-      },
-			onStompError: (error) => {
-				console.log(error)
-			}
+      onConnect,
+      onDisconnect,
+			onStompError
     });
 
     stompClient.activate();
@@ -47,11 +53,12 @@ const useStomp = (url: string, publish: string, subscribe: string) => {
 
   onUnmounted(() => stompClient.deactivate());
 
+
   return { 
+    errors,
 		messages, 
 		sendMessage 
 	}
 };
-
 
 export default useStomp;
